f25eebe3c19c3926633da2f806bee851baafbc17
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-20 14:23:46 +0800

    vercel error fixed

--- front-end/app/page.js
+++ front-end/app/page.js
@@ -1,8 +1,7 @@
 import Initial from "@/components/modal/Initial";
 import { getBlogs } from "@/lib/blogs/getBlogs";
 export default async function Home() {
-  const res= await getBlogs()
-  return (
-   <Initial res={res} />
-  );
+  const res = await getBlogs();
+  return <Initial res={res} />;
 }
+// 1. Vercel back-end point was not matched . So error is fixed .
